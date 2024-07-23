90dd9c29a3909cf6e735502247a055bcf4aa720f
Bom <lee.bom.952@gmail.com>
2024-07-23 09:43:02 +0900

    Update Q120802

---
+++ src/q120802/Solution.java
@@ -0,0 +1,11 @@
+package q120802;
+
+// 두 수의 합
+// https://school.programmers.co.kr/learn/courses/30/lessons/120802
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+}
