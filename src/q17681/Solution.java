e4faf5d219096697aa7c14d2273b8a3c61c3be3e
everydayspring <lee.bom.952@gmail.com>
2024-09-15 15:36:30 +0900

    study 240915

---
+++ src/q17681/Solution.java
@@ -0,0 +1,12 @@
+package q17681;
+
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+
+        for(int num : arr1){
+            System.out.println(Integer.parseInt(Integer.toString(num), 2));
+        }
+        return answer;
+    }
+}
